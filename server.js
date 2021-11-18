0f2ba2f979d47158b4db8be6787252e354845891
samorvell <samuel_amaro_4@hotmail.com>
2021-11-18 08:52:49 -0300

    atualização frontjs

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require ('express')
 const app = express()
+const path = require('path')
+const porta = 8082
 
 
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/src/index.html")
+})
+
+app.get("/cadastroEmpresa", function(req, res){
+    res.sendFile(__dirname + "/src/cadastroEmpresa.html")
+})
+
+app.get("/cadastroFuncionario", function(req, res){
+    res.sendFile(__dirname + "/src/cadastroFuncionario.html")
+})
+
+app.use(express.static(path.join(__dirname,"public")));
 
 
-app.listen('4567')
+app.listen(porta, () => {
+    console.log(`Servidor execultando na porta: ${porta}`)
+})
