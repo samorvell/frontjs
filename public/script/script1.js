a4b54cd4647ec2eaeffaa745ea052ce677240eda
Yan Alejandro <ysilva@idealit.com.br>
2021-11-29 15:03:52 -0300

    implementacao tabela de registro

---
+++ public/script/script1.js
@@ -0,0 +1,15 @@
+class CadastroFuncionario{
+
+    listaTabela(){
+        let tbody = document.getElementById('tbody');
+      
+        for(let i = 0; i < his.arrayProdutos.length; i++){
+            let th = tbody.insertRow();
+      
+            let td_id = tr.insertCell();
+            let td_data = tr.insertCell();
+            let td_tipo = tr.insertCell();
+            let td_acoes = tr.insertCell();
+        }
+    }
+}
